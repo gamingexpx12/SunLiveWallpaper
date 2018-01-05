a2bfae7836f77f6a82da7e21d13d30397fde8855
gamingexpx12 <gamingexpx12@gmail.com>
2018-01-05 15:38:58 +0100

    Added project files

---
+++ helpers.js
@@ -0,0 +1,72 @@
+function dateToSeconds(date) {
+  return date.getHours() * 3600 + date.getMinutes() * 60 + date.getSeconds()
+}
+function SecondsToDate(secs) {
+  return new Date(2017, 10, 31, secs / 3600, (secs % 3600) / 60, secs % 60, 0)
+}
+function AddStars(selection, amount) {
+  for (var i = 0; i < amount; i++) {
+    selection.append("circle")
+    .attr("r", 1)
+    .attr("cx", function () {return Math.random() * 1445})
+    .attr("cy", function () {return Math.random() * 1445})
+  }
+}
+
+function getSunriseSunsetTimes() {
+    getIPAdress(function (ip) {
+        getLatLonFromIP(ip, function (lat, lon) {
+            getSunriseSunsetFromLatLon(lat, lon, function (resp) {
+                console.log(resp);
+                sunResults = resp.results
+                sunrise = dateToSeconds(new Date(resp.results.sunrise))
+                sunset = dateToSeconds(new Date(resp.results.sunset))
+                dawn = dateToSeconds(new Date(resp.results.civil_twillight_begin))
+                dusk = dateToSeconds(new Date(resp.results.civil_twillight_end))
+            })
+        })
+    })
+}
+
+function getIPAdress(callback) {
+    d3.text("https://api.ipify.org", function (error, ip) {
+        if (error) throw error;
+        callback(ip)
+    })
+}
+
+function getLatLonFromIP(ip, callback) {
+    d3.json("http://ip-api.com/json/" + ip +"?fields=lat,lon,status,message", function (error, latLonStatus) {
+        if (error) throw error;
+        if (latLonStatus.status == "success") {
+            console.log("Lat Lon request successfull")
+            callback(latLonStatus.lat, latLonStatus.lon)
+        } else {
+            console.error("Lat Lon request failed")
+        }
+    })
+}
+
+function getSunriseSunsetFromLatLon(lat, lon, callback) {
+    var apipre = "https://api.sunrise-sunset.org/json?lat="
+    var apimid = "&lng="
+    var apisuf = "&date=today&formatted=0"
+    var sunrequest = apipre + lat + apimid + lon + apisuf
+
+    d3.json(sunrequest, function (error, resp) {
+        if (error) throw error;
+        callback(resp)
+    })
+}
+
+function Time(date) {
+  this.date = date
+  this.h = date.getHours()
+  this.m = date.getMinutes()
+  this.s = date.getSeconds()
+  this.totalSeconds = dateToSeconds(date)
+  this.sunrise = sunrise
+  this.sunset = sunset
+  this.fraction = d3.scaleLinear().domain([this.sunrise, this.sunset])(this.totalSeconds)
+
+}
---
+++ script.js
@@ -0,0 +1,94 @@
+var sunAPI = "https://api.sunrise-sunset.org/json?lat=58.943598&lng=5.714572&date=today&formatted=0"
+var locale = "en-GB"
+var svg = d3.select("svg").style("z-index", "1")
+var stargroup = svg.insert("g", "#grass").attr("id", "stargroup")
+var sunResults = ""
+var sunrise = 21600
+var sunset = 64800
+var dawn = 20600
+var dusk = 65800
+var starangle = 0
+const tau = 2 * Math.PI
+
+//Debugtime slider
+var debugTime = new Time(new Date(2017, 10, 31, 7, 20, 1, 0));
+/*d3.select("div").append("input")
+        .attr("type", "range")
+        .attr("min", "0")
+        .attr("max", "86400")
+        .on("input", function () {
+          debugTime = new Time(SecondsToDate(this.value))
+        })
+*/
+var sun = d3.select("#sun")
+d3.select("html").style("overflow", "hidden")
+//Make Digital clock
+var clock = d3.select("svg").append("text")
+    .text("13:33:37")
+    .attr("x", "50%").attr("y", "70%")
+    .attr("fill", "white").attr("text-anchor", "middle")
+    .style("font", "bold 100px Verdana, Helvetica, Arial, sans-serif")
+
+
+
+
+var newtime = new Time(new Date())
+
+function update() {
+    newtime = new Time(new Date())
+    //newtime = debugTime
+
+    clock.text(newtime.date.toLocaleTimeString(locale)) //Digital clock
+    //Move sun along path
+    var sunpathElement = d3.select("#sunpath").node()
+    var sunpathLength = sunpathElement.getTotalLength()
+    if (newtime.fraction < 1 && newtime.fraction > 0) {
+        var sunpathPoint = sunpathElement.getPointAtLength(newtime.fraction * sunpathLength)
+        sun.attr("cx", sunpathPoint.x).attr("cy", sunpathPoint.y)
+        //More color hacking
+        sun.attr("fill", "orange")
+        d3.select("#grass").attr("fill", "seagreen")
+        d3.select("#sky").attr("fill", "hsl(208, 44%, 70%)")
+        stargroup.attr("fill", "none")
+        //Color hacking
+        if (newtime.fraction > 0.95 || newtime.fraction < 0.05) {
+            sun.attr("fill", "red")
+            d3.select("#sky").attr("fill", "orange")
+            d3.select("#grass").attr("fill", "#20a420")
+        }
+    } else {
+        sun.attr("fill", "none")
+        d3.select("#grass").attr("fill", "darkgreen")
+        d3.select("#sky").attr("fill", "black")
+        stargroup.attr("fill", "white")
+        stargroup.attr("transform", "rotate(" + starangle +", 960, 1920)")
+        starangle = starangle + 0.1
+    }
+}
+// MORE HACKING!!!
+function AddSunpath(selection) {
+    let g = selection.append("g")
+        .attr("transform", "translate(" + (1920 / 2) + "," + (1080 / 2) + ")")
+    let path = g.append("path").attr("d", "m -50 -50 a 25 25 -30 0 1 100 0 a 25 25 -30 0 1 -100 0")
+            .attr("transform", "scale(1)")
+            .style("fill", "none")
+            .attr("stroke", "red")
+            .attr("stroke-width", "5")
+            let sunpathElement = path.node()
+            let sunpathL = sunpathElement.getTotalLength()
+            console.log(sunpathL)
+            let sunpathPoint = sunpathElement.getPointAtLength(2 * sunpathL)
+            console.log(sunpathPoint)
+            svg.append("circle")
+            .attr("r", 20)
+            .attr("cx", sunpathPoint.x)
+            .attr("cy", sunpathPoint.y)
+            .attr("fill", "yellow")
+}
+
+
+getSunriseSunsetTimes()
+AddStars(stargroup, 500)
+AddSunpath(svg)
+update()
+setInterval(update, 100)
