93da3cb122af8ad481e396b62e0ca80461f02ab7
gamingexpx12 <gamingexpx12@gmail.com>
2018-01-14 12:41:31 +0100

    Moved svg drawing to script and made proper layers

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,7 @@
 var sunAPI = "https://api.sunrise-sunset.org/json?lat=58.943598&lng=5.714572&date=today&formatted=0"
 var locale = "en-GB"
-var svg = d3.select("svg")
-var stargroup = svg.insert("g", "#grass").attr("id", "stargroup")
+//var svg = d3.select("svg")
+//var stargroup = svg.insert("g", "#grass").attr("id", "stargroup")
 var sunResults = ""
 var sunrise = 21600
 var sunset = 64800
@@ -12,6 +12,13 @@ const tau = 2 * Math.PI
 
 var debugTime = new Time(new Date(2017, 10, 31, 7, 20, 1, 0));
 
+const svg = d3.select("div").append("svg")
+    .attr("id", "mainsvg")
+    .attr("version", "1.1")
+    .attr("viewbox", "0 0 1920 1080")
+    .attr("width", "100%")
+    .attr("height", "1080px")
+
 d3.select("html").style("overflow", "hidden")
 //Make Digital clock
 var clock = d3.select("svg").append("text")
@@ -20,6 +27,27 @@ var clock = d3.select("svg").append("text")
     .attr("fill", "white").attr("text-anchor", "middle")
     .style("font", "bold 100px Verdana, Helvetica, Arial, sans-serif")
 
+//Add background(sky)
+const back = svg.insert("rect",":first-child")
+    .attr("id", "sky")
+    .attr("height", "100%")
+    .attr("width", "100%")
+    .attr("fill", "deepskyblue")
+
+//Add foreground(grass)
+const fore = svg.insert("rect", ":last-child")
+    .attr("id", "grass")
+    .attr("x", "0px")
+    .attr("y", "70%")
+    .attr("height", "30%")
+    .attr("width", "100%")
+    .attr("fill", "seagreen")
+
+//Add stars
+const stars = svg.insert("g", "#grass")
+    .attr("id", "stargroup")
+AddStars(stars, 500)
+//Add sun
 const newsun = new Sun(svg, window.outerWidth * 0.5, window.outerHeight * 0.7 + 100, 600).sunradius(100)
 svg.insert(() => newsun.svg.node(), "#grass")
 var newtime = new Time(new Date())
@@ -37,7 +65,7 @@ function update() {
         //More color hacking
         d3.select("#grass").attr("fill", "seagreen")
         d3.select("#sky").attr("fill", "hsl(208, 44%, 70%)")
-        stargroup.attr("fill", "none")
+        stars.attr("fill", "none")
         //Color hacking
         if (newtime.fraction > 0.95 || newtime.fraction < 0.05) {
             d3.select("#sky").attr("fill", "orange")
@@ -46,13 +74,13 @@ function update() {
     } else {
         d3.select("#grass").attr("fill", "darkgreen")
         d3.select("#sky").attr("fill", "black")
-        stargroup.attr("fill", "white")
-        stargroup.attr("transform", "rotate(" + starangle +", 960, 1920)")
+        stars.attr("fill", "white")
+        stars.attr("transform", "rotate(" + starangle +", 960, 1920)")
         starangle = starangle + 0.1
     }
 }
 
 getSunriseSunsetTimes()
-AddStars(stargroup, 500)
+
 update()
 setInterval(update, 100)
